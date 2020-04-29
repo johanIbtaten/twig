2e1895e704d1514f92b75a6a35912978974a3496
johanIbtaten <48206606+johanIbtaten@users.noreply.github.com>
2020-04-29 15:47:24 +0200

    Added Twig examples

--- MonExtension.php
+++ MonExtension.php
@@ -1,30 +1,49 @@
 <?php
 
+/* On crée une classe MonExtension qui étend la classe Twig_Extension */
 class MonExtension extends Twig_Extension
 {
 
+    // On déclare les filters
     public function getFilters()
     {
         return [
-            new Twig_SimpleFilter('markdown', [$this, 'markdownParse'], ['is_safe' => ['html']])
+            /* Le filter twig aura pour nom markdownFilter, il utilise la
+            méthode markdownParse() pour retourner le résultat du filtre.
+            Le résultat html ne sera pas échappé par twig grâce à l'option
+            ['is_safe' => ['html'] */
+            new Twig_SimpleFilter('markdownFilter', [$this, 'markdownParse'], ['is_safe' => ['html']])
         ];
     }
 
+    // On déclare les fonctions
     public function getFunctions()
     {
         return [
+            /* Le fonction twig aura pour nom markdownFunction, elle utilise
+            la méthode markdownParse() pour retourner le résultat */
+            new Twig_SimpleFunction('markdownFunction', [$this, 'markdownParse'], ['is_safe' => ['html']]),
+            /* Le fonction twig aura pour nom activeClass, elle utilise
+            la méthode activeClass() pour retourner le résultat.
+            La méthode aura accès en paramètre au context du template 
+            qui l'appelle */
             new Twig_SimpleFunction('activeClass', [$this, 'activeClass'], ['needs_context' => true])
         ];
     }
 
     public function markdownParse($value)
     {
+        /* On retrourne la string transformé par la méthode du package 
+        \Michelf\MarkdownExtra::defaultTransform() */
         return \Michelf\MarkdownExtra::defaultTransform($value);
     }
 
     public function activeClass(array $context, $page) {
+        /* Si la variable globale current_page existe et que sa valeur
+        est égale à la valeur définit dans le lien */
         if (isset($context['current_page']) && $context['current_page'] === $page) {
-            return ' active ';
+          /* Alors on retourne la classe active */  
+          return ' active ';
         }
     }
 }
--- index.php
+++ index.php
@@ -1,6 +1,47 @@
 <?php
+// Indispensable pour que twig fonctionne
 require 'vendor/autoload.php';
+
+// Importe mon extension personnalisée
 require 'MonExtension.php';
+
+// Rendu du template
+// Va chercher les fichiers de template dans le dossier /templates
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
+$twig = new Twig_Environment($loader, [
+    'cache' => false, // Mettre à la place de false en production  __DIR__ . '/tmp', 
+    'debug' => true
+]);
+
+// Ajoute mon extension personnalisée à twig
+$twig->addExtension(new MonExtension());
+
+// Permet d'activer le debug avec {{ dump() }} dans les templates
+$twig->addExtension(new Twig_Extension_Debug());
+
+// Après un composer require twig/extensions
+// permet d'importer l'extension Text de twig
+$twig->addExtension(new Twig_Extensions_Extension_Text());
+
+
+// $twig->addFunction(new Twig_SimpleFunction(
+//   'markdownFunction', 
+//   function($value) { 
+//     return \Michelf\MarkdownExtra::defaultTransform($value);
+//   },
+//   ['is_safe' => ['html']]
+//   )
+// );
+
+// $twig->addFilter(new Twig_SimpleFilter(
+//   'markdownFilter', 
+//   function($value) { 
+//     return \Michelf\MarkdownExtra::defaultTransform($value);
+//   },
+//   ['is_safe' => ['html']]
+//   )
+// );
+
 
 // Routing
 $page = 'home';
@@ -8,37 +49,93 @@ if (isset($_GET['p'])) {
     $page = $_GET['p'];
 }
 
-// Récupère les derniers tutoriels
-// function tutoriels () {
-//     $pdo = new PDO('mysql:dbname=grafikart_dev;host=localhost', 'root', 'root');
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-//     $tutoriels = $pdo->query('SELECT * FROM tutoriels ORDER BY id DESC LIMIT 10');
-//     return $tutoriels;
-// }
-
-// Rendu du template
-$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates');
-$twig = new Twig_Environment($loader, [
-    'cache' => __DIR__ . '/tmp',
-    'debug' => true
-]);
-
-$twig->addExtension(new MonExtension());
-$twig->addExtension(new Twig_Extension_Debug());
-// $twig->addExtension(new Twig_Extensions_Extension_Text());
+/* Ajoute la variable $page en global, elle devient 
+disponible dans tous les templates */
 $twig->addGlobal('current_page', $page);
 
 switch ($page) {
-    case 'contact':
-        echo $twig->render('contact.twig', ['name' => 'Marc', 'email' => 'demo@demo.fr']);
+  // Si on est sur la page contact
+  case 'contact':
+      // On fait le rendu de contact.twig
+      echo $twig->render(
+        'contact.twig',
+        /* On passe des variables au template sous la forme 
+        d'un tableau associatif */
+        [
+          'name' => 'Marc', 
+          'email' => 'demo@demo.fr'
+        ]
+      );
+    break;
+  case 'home':
+    echo $twig->render('home.twig', [
+      'email' => '<strong>marc@gmail.com</strong>', 
+      'user' => [
+        'name' => 'John',
+        'age' => 28
+      ],
+      'persons' => [
+        [
+          'id' => 1269,
+          'name' => 'Marc',
+          'age' => 35,
+          'description' => 'Peter Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1268,
+          'name' => 'Peter',
+          'age' => 37,
+          'description' => 'Peter Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1158,
+          'name' => 'Ben',
+          'age' => 43,
+          'description' => 'Ben Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1269,
+          'name' => 'Marc',
+          'age' => 35,
+          'description' => 'Peter Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1268,
+          'name' => 'Peter',
+          'age' => 37,
+          'description' => 'Peter Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1158,
+          'name' => 'Ben',
+          'age' => 43,
+          'description' => 'Ben Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1269,
+          'name' => 'Marc',
+          'age' => 35,
+          'description' => 'Peter Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1268,
+          'name' => 'Peter',
+          'age' => 37,
+          'description' => 'Peter Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ],
+        [
+          'id' => 1158,
+          'name' => 'Ben',
+          'age' => 43,
+          'description' => 'Ben Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum possimus sapiente architecto nobis, debitis vel qui cum consectetur enim iste, natus nam reprehenderit accusantium! Fugit quos perspiciatis architecto veniam sint!'
+        ]
+      ]
+    ]);
+    
+      // echo $twig->render('home.twig', ['tutoriels' => tutoriels()]);
       break;
-      case 'home':
-        echo $twig->render('main.twig');
-        // echo $twig->render('home.twig', ['tutoriels' => tutoriels()]);
-        break;
-    default:
-        header('HTTP/1.0 404 Not Found');
-        echo $twig->render('404.twig');
-        break;
+  default:
+      header('HTTP/1.0 404 Not Found');
+      echo $twig->render('404.twig');
+      break;
 }
